c4ff46a929d3373f8ca0015c46608c34c8177300
csmfindling <charles.findling@gmail.com>
2017-06-28 20:55:06 +0200

    remove renamed directory

--- code/code/exploration/rBayesianOptimization-master/R/rBayesianOptimization.R
+++
@@ -1,7 +0,0 @@
-#' rBayesianOptimization: Bayesian Optimization of Hyperparameters
-#'
-#' A Pure R implementation of bayesian global optimization with gaussian processes.
-#'
-#' @docType package
-#' @name rBayesianOptimization
-NULL
